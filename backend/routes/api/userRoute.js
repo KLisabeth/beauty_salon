5fac679570faf5174afb7150518203b341e246ab
Katie <katie_lisabeth@yahoo.com>
2020-12-09 10:34:19 +0100

    register user

--- backend/routes/api/userRoute.js
+++ backend/routes/api/userRoute.js
@@ -1,5 +1,6 @@
 const express = require ("express");
 const User = require ('../../models/userModel');
+const { getToken } = require ('../../config/utils');
 
 
 const router = express.Router();
@@ -19,6 +20,29 @@ const newUser = await user.save();
     res.send({msg: error.message})
 }
 });
+router.post('/register', async (req, res)=>{
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        mobile: req.body.mobile,
+        password: req.body.password
+        
+    })
+    const newUser = await user.save()
+    if(newUser){
+        res.send({
+            _id: newUser.id,
+            name: newUser.name,
+            email: newUser.email,
+            mobile: newUser.mobile,
+            isAdmin: newUser.isAdmin,
+            token: getToken(newUser)
+        })
+    }else{
+        res.status(401).send({msg: 'Invalid user data'})
+    }
+    
+    })
 
 
 module.exports = router;
